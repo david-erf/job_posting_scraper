1620f6e4661de765f667f063004a25f9dcd6fe88
David Erf <13736190+david-erf@users.noreply.github.com>
2025-02-19 19:12:28 -0800

    reorganize more utils

--- scripts/get_descriptions.py
+++ scripts/get_descriptions.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import time
-from datetime import date
+from datetime import date,datetime
 today = date.today()
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 import requests
 import pickle
-from utils import get_jobs_for_keyword,convert_to_days,split_location,mapping,abbreviation_mapping,rank_job_posting,keywords
-
+from utils import get_jobs_for_keyword,convert_to_days,split_location,mapping,abbreviation_mapping,rank_job_posting,keywords,parse_job_id
 from bs4 import BeautifulSoup
 import re
 from dotenv import load_dotenv
@@ -17,6 +17,7 @@ load_dotenv()  # Loads .env variables into environment
 resume_path = os.getenv("RESUME_PATH")  # Now accessible like an env var
 with open(resume_path, "r") as file:
     resume = file.read()
+    resume = resume + ", ".join(keywords)
 
 # Get JOb Descriptions for keywords 
 results={}
@@ -32,7 +33,7 @@ for k in results.keys():
   df['searched_keyword']= k
   l.append(df)
 results_df = pd.concat(l,axis=0)
-results_df.to_csv(f'../data/job_search_results_{today}.csv')
+results_df.to_csv(f'../data/job_search_results_{timestamp}.csv')
 
 
 # master_path = '../data/master_jobs.pkl'
@@ -43,15 +44,15 @@ results_df.to_csv(f'../data/job_search_results_{today}.csv')
 #     master_jobs_dict = pickle.load(file)
 
 
-# results_df=pd.read_csv(f'../data/job_search_results_{today}.csv')
+# results_df=pd.read_csv(f'../data/job_search_results_{timestamp}.csv')
 results_df['company'].fillna('', inplace=True)
 results_df.drop_duplicates(subset=['title','company','searched_keyword','job_url'], inplace=True)
 results_df['title_relevance'] = results_df['title'].apply(lambda x: rank_job_posting(resume, x))
-results_df.to_csv(f'../data/job_search_title_relevance_{today}.csv')
+results_df.to_csv(f'../data/job_search_title_relevance_{timestamp}.csv')
 
   
-# to do: if not rerun
-# results_df = pd.read_csv(f'../data/job_search_title_relevance_{today}.csv')
+# # to do: if not rerun
+# results_df = pd.read_csv(f'../data/job_search_title_relevance_{timestamp}.csv')
 
 # todo: decide if we want to increase relevance threshold
 threshold = 1
@@ -59,119 +60,22 @@ bool_relevant = (results_df['title_relevance']>=threshold)
 matched_results_df = results_df[bool_relevant].copy()
 print('count of jobs',matched_results_df.shape)
 # Loop through all job IDs and pull back html content
-jobs_raw = {}
+jobs = {}
 ct=0
 for job_id,url in list(zip(matched_results_df['job_id'],matched_results_df['job_url'])):
-  print(url)
-  ct+=1 
-  print(ct)
+  jobs[job_id] = parse_job_id(job_id,url,resume)
   time.sleep(2)
-  try:
-    response = requests.get(url, timeout=20)
-  except:
-    response = 'failed to pull'
-  jobs_raw[job_id] = {
-        'raw_response':response
-        }
-
-# with open(f'../data/job_descriptions_raw_{today}.pkl', "rb") as file:
-#     jobs_raw = pickle.load(file)
-
-jobs=jobs_raw.copy()
-
-# Regular expression to find dollar amounts
-dollar_pattern = r'\$\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?'
-
-# Parse Soup for each Job
-ct=0
-for k in list(jobs.keys()):
-  ct+=1
-  print(ct)
-  criteria = {}
-  try:
-    soup = BeautifulSoup(jobs[k]['raw_response'].content, "html.parser")
-    # Get date posted
-    date_posted_tag = soup.find('span', class_='posted-time-ago__text')
-    if date_posted_tag:
-      jobs[k].update({'date_posted':date_posted_tag.get_text(strip=True)})
-
-    # Get number of applicants
-    num_applicants_tag = soup.find('figcaption', class_='num-applicants__caption')
-    if num_applicants_tag:
-      jobs[k].update({'num_applicants':num_applicants_tag.get_text(strip=True)})
-
-    # Find the job description information
-    job_description_div = soup.find('div', class_='show-more-less-html__markup')
-    if job_description_div:
-        job_description = job_description_div.text.strip()
-        jobs[k].update({'job_description': job_description})
-        
-        # Get Relevance
-        relevance= rank_job_posting(resume, job_description)
-        jobs[k].update({'job_description_relevance':relevance})
 
 
-    # Get Header information
-    for item in soup.find_all('li', class_='description__job-criteria-item'):
-        header = item.find('h3', class_='description__job-criteria-subheader').get_text(strip=True)
-        value = item.find('span', class_='description__job-criteria-text').get_text(strip=True)
-        criteria[header] = value
-        jobs[k].update(criteria)
-        # print( criteria)
+# with open(f'../data/job_descriptions_raw_{timestamp}.pkl', "rb") as file:
+#   jobs_raw = pickle.load(file)
 
-    # Get Location Information
-    og_title = soup.find('meta', property='og:title')
-    if og_title:
-        title_content = og_title['content']
-        jobs[k].update({'title_content': title_content})
-        # Extract the location from the title content
-        if "in " in title_content:
-            location = title_content.split("in ")[-1].split("|")[0].strip()
-            jobs[k].update({'location': location})
+# jobs=jobs_raw.copy()
 
-    # Get Salary information
-    salary_div = soup.find('div', class_='compensation__salary')
-    if salary_div:
-        salary = salary_div.text.strip()
-        if "/yr" in salary:
-          salary_frequency='annual'
-        elif "/hr" in salary:
-          salary_frequency='hourly'
-        elif "/mo" in salary:
-          salary_frequency='monthly'
-        else:
-          salary_frequency='unknown'
-        jobs[k].update({'salary_frequency':salary_frequency})
-        min_salary, max_salary = salary.replace("$", "").replace("/yr", "").replace('/hr',"").replace("/mo","").split(" - ")
-        min_salary = float(min_salary.replace(",", ""))
-        max_salary = float(max_salary.replace(",", ""))
-        jobs[k].update({'min_salary':min_salary,
-                        'max_salary':max_salary,
-                        'salary_range':salary})
-    # If salary isn't in the expected field, check for dollar values in the job description
-    else:
-        # if the field isn't explicit,
-        dollar_amounts = re.findall(dollar_pattern, job_description)
-        dollar_amounts = [float(s.replace("$", "").replace(",", "")) for s in dollar_amounts]
-        # todo
-        if len(dollar_amounts)>0:
-          max_salary=max(dollar_amounts) # should be safe
-          min_salary=min([s for s in dollar_amounts if s>max_salary*.1]) # but handle cases where annualized income and hourly are intermingled (prefer annualized)
-
-          # handle likely data integrity issues
-          # todo: replace with configurable variable
-          if min_salary>500000:
-            min_salary=None
-          if max_salary>500000:
-            max_salary=None
-          jobs[k].update({'min_salary':min_salary,
-                          'max_salary':max_salary})
-  except:
-    print(f'failed on {k}')
 
 
 # Save dictionary as a pickle file
-with open(f'../data/job_descriptions_{today}.pkl', "wb") as file:
+with open(f'../data/job_descriptions_{timestamp}.pkl', "wb") as file:
     pickle.dump(jobs, file)
 
 
@@ -204,7 +108,7 @@ jobs_df['state'] = jobs_df['clean_location'].apply(lambda x: x[1])
 # Process Date Posted
 jobs_df['time_since_posted'] = jobs_df['date_posted'].apply(lambda x: convert_to_days(x))
 
-jobs_df.to_csv(f'../data/formatted_jobs_df_{today}.csv')
+jobs_df.to_csv(f'../data/formatted_jobs_df_{timestamp}.csv')
 
 # quality checks
 # jobs_df.groupby(['state']).agg({'id':'count'}).sort_values('id',ascending=False).head(100)
--- scripts/utils.py
+++ scripts/utils.py
@@ -8,7 +8,7 @@ from datetime import date
 today = date.today()
 import subprocess
 
-keywords = ['data science','data analytics','fintech','AI intern',"ML Intern"]
+keywords = ['data science','data analytics','fintech','AI intern',"ML Intern","Salesforce",'Salesforce AI']
 
 
 def get_jobs_for_keyword(keyword, pages=10,pause=2):
@@ -41,6 +41,18 @@ def get_jobs_for_keyword(keyword, pages=10,pause=2):
         for card in job_cards:
             title = card.find('h3').text.strip() if card.find('h3') else None
             company = card.find('a', class_='hidden-nested-link').text.strip() if card.find('a', class_='hidden-nested-link') else None
+            # Extract date posted
+            date_posted_elem = card.find_previous('time')  # Looks for the closest preceding <time> tag
+            date_posted = date_posted_elem.text.strip() if date_posted_elem else None
+
+            # Extract location
+            location_elem = card.find('span', class_='job-search-card__location')
+            location = location_elem.text.strip() if location_elem else None
+
+            # Extract info about applicant count
+            hiring_status_elem = card.find('span', class_='job-posting-benefits__text')
+            hiring_status = hiring_status_elem.text.strip() if hiring_status_elem else None
+
             # Removed the location extraction
             if card.parent:
               job_id = card.parent.get('data-entity-urn').split(':')[-1]
@@ -50,15 +62,16 @@ def get_jobs_for_keyword(keyword, pages=10,pause=2):
               job_url = None
 
             jobs.append({
-                'title': title,
-                'company': company,
-                'job_id':job_id,
-                'job_url': job_url,
+            'title': title,
+            'company': company,
+            'date_posted': date_posted, 
+            'location': location,       
+            'job_id': job_id,
+            'job_url': job_url,
+            'hiring_status':hiring_status,
             })
 
         time.sleep(pause)
-
-
     return jobs
 
 def call_ollama_run(model: str, prompt: str) -> str:
@@ -230,3 +243,125 @@ abbreviation_mapping = {
     "New York": "NY",
 
 }
+
+def parse_job_id(job_id,url,resume):
+
+    ans = {}
+    # Regular expression to find dollar amounts
+    dollar_pattern = r'\$\d{1,3}(?:,\d{3})*(?:\.\d{1,2})?'
+
+    try:
+        response = requests.get(url, timeout=20)
+    except:
+        response = 'failed to pull'
+
+    ans.update({'raw_response':response})
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Get date posted
+    date_posted_tag = soup.find('span', class_='posted-time-ago__text')
+    if date_posted_tag:
+      ans.update({'date_posted':date_posted_tag.get_text(strip=True)})
+
+    # Get number of applicants
+    num_applicants_tag = soup.find('figcaption', class_='num-applicants__caption')
+    if num_applicants_tag:
+      ans.update({'num_applicants':num_applicants_tag.get_text(strip=True)})
+
+    # Find the job description information
+    job_description_div = soup.find('div', class_='show-more-less-html__markup')
+    if job_description_div:
+        job_description = job_description_div.text.strip()
+        ans.update({'job_description': job_description})
+        
+        # Get Relevance
+        relevance= rank_job_posting(resume, job_description)
+        ans.update({'job_description_relevance':relevance})
+
+
+    # Get Header information
+    for item in soup.find_all('li', class_='description__job-criteria-item'):
+        header = item.find('h3', class_='description__job-criteria-subheader').get_text(strip=True)
+        value = item.find('span', class_='description__job-criteria-text').get_text(strip=True)
+        ans.update({header: value})
+
+    # Get Location Information
+    og_title = soup.find('meta', property='og:title')
+    if og_title:
+        title_content = og_title['content']
+        ans.update({'title_content': title_content})
+        # Extract the location from the title content
+        if "in " in title_content:
+            location = title_content.split("in ")[-1].split("|")[0].strip()
+            ans.update({'location': location})
+
+    # Get Salary information
+    salary_div = soup.find('div', class_='compensation__salary')
+    if salary_div:
+        salary = salary_div.text.strip()
+        if "/yr" in salary:
+          salary_frequency='annual'
+        elif "/hr" in salary:
+          salary_frequency='hourly'
+        elif "/mo" in salary:
+          salary_frequency='monthly'
+        else:
+          salary_frequency='unknown'
+        ans.update({'salary_frequency':salary_frequency})
+        min_salary, max_salary = salary.replace("$", "").replace("/yr", "").replace('/hr',"").replace("/mo","").split(" - ")
+        min_salary = float(min_salary.replace(",", ""))
+        max_salary = float(max_salary.replace(",", ""))
+        ans.update({'min_salary':min_salary,
+                        'max_salary':max_salary,
+                        'salary_range':salary})
+    # If salary isn't in the expected field, check for dollar values in the job description
+    else:
+        # if the field isn't explicit,
+        dollar_amounts = re.findall(dollar_pattern, job_description)
+        dollar_amounts = [float(s.replace("$", "").replace(",", "")) for s in dollar_amounts]
+        # todo
+        if len(dollar_amounts)>0:
+          max_salary=max(dollar_amounts) # should be safe
+          min_salary=min([s for s in dollar_amounts if s>max_salary*.1]) # but handle cases where annualized income and hourly are intermingled (prefer annualized)
+
+          # handle likely data integrity issues
+          # todo: replace with configurable variable
+          if min_salary>500000:
+            min_salary=None
+          if max_salary>500000:
+            max_salary=None
+          ans.update({'min_salary':min_salary,
+                          'max_salary':max_salary})
+    # except:
+    #     print(f'failed on {job_id}')
+    return ans
+
+def write_cover_letter(resume: str, job_posting: str,lim:int = 2000) -> int:
+    """
+    Uses the Ollama CLI (with the Mistral model) to write a cover letter
+    
+    Parameters:
+        resume (str): The candidate's resume.
+        job_posting (str): The job posting description or title.
+        
+    Returns:
+        int: The relevance score.
+    """
+    resume=resume[0:lim]
+    job_posting=job_posting[0:lim]
+    
+    prompt = f"""You are an AI professional cover letter writer. For each input, return a cover letter that adapts my Resume to the Job Posting. Answer the question: why does David want to work here?
+
+    ### Resume:
+    {resume}
+
+    ### Job Posting:
+    {job_posting}
+
+    """
+    prompt=prompt[0:4000]
+    try:
+        response = call_ollama_run("mistral", prompt)
+    except Exception as e:
+        print(f"Error processing job posting: {job_posting}\nError: {e}")
+
+    return response
